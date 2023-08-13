bb8256a3f547d122c9cd0c36b2e38f47d241e330
a0m0snj <ayush.maheshwari@walmart.com>
2023-08-13 14:57:26 +0530

    Fixing build

--- app/page.tsx
+++ app/page.tsx
@@ -88,7 +88,7 @@ export default function Home() {
         ) : (
           <div className="mt-16 flex justify-center items-center flex-col gap-2">
             <h2 className="text-black text-xl font-bold">Oops, no results</h2>
-            <p>{allCars?.message}</p>
+            {/* <p>{allCars?.message}</p> */}
           </div>
         )}
       </div>
